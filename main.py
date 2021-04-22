ccaf3fdbb5377fc38cf9d5d565987c942b69ae24
Viktor <viktor.malakhov05@gmail.com>
2021-04-22 13:28:25 +0500

    Задание №12 + fix №11 + fix №4 + fix №7

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import math
+from pprint import pprint
+
 import requests
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel
 from PyQt5.QtCore import Qt
 from PyQt5 import uic
 
@@ -29,6 +31,21 @@ class ShowMap(QMainWindow):
         self.full_address = ''
         self.post = ''
         self.initUI()
+        self.widget = None
+        self.help_btn.clicked.connect(self.get_help)
+
+    def lonlat_distance(self, a, b):
+
+        degree_to_meters_factor = 111 * 1000
+        a_lon, a_lat = a
+        b_lon, b_lat = b
+
+        radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+        lat_lon_factor = math.cos(radians_lattitude)
+        dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+        dy = abs(a_lat - b_lat) * degree_to_meters_factor
+        distance = math.sqrt(dx * dx + dy * dy)
+        return distance
 
     def add_post_code(self):
         if self.postal_code.isChecked():
@@ -124,7 +141,7 @@ class ShowMap(QMainWindow):
             math.radians(self.y)) * math.pow(2, 15 - self.z)
         return lx, ly
 
-    def get_pos(self, address):
+    def get_pos(self, address, stay=False):
         params = {"apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
                   "geocode": address,
                   "format": "json"}
@@ -153,18 +170,98 @@ class ShowMap(QMainWindow):
             if self.postal_code.isChecked():
                 r += ' ' + self.post
             self.adress.setText(r)
-            self.y, self.x = tuple(map(float, toponym['Point']['pos'].split()))
-            self.pt = f'{self.y},{self.x},comma'
-            return
+            if not stay:
+                self.y, self.x = tuple(
+                    map(float, toponym['Point']['pos'].split()))
+                self.pt = f'{self.y},{self.x},comma'
+                return
+            else:
+                y, x = tuple(map(float, toponym['Point']['pos'].split()))
+                pt = f'{y},{x},comma'
+                return pt
         print("Ошибка выполнения запроса:")
         print(geocoder_request)
         print("Http статус:", response.status_code, "(", response.reason, ")")
 
+    def find_organiztion(self, pos):
+        req = 'https://search-maps.yandex.ru/v1/'
+        address_ll = f'{pos[1]},{pos[0]}'
+        adr = self.get_address(address_ll)
+        params = {'apikey': 'dda3ddba-c9ea-4ead-9010-f43fbc15c6e3',
+                  "text": adr,
+                  "lang": "ru_RU",
+                  "ll": address_ll,
+                  "type": "biz"
+                  }
+        response = requests.get(req, params=params)
+        json_response = response.json()
+        if response:
+            organization = json_response["features"][0]
+            x, y = organization['geometry']['coordinates']
+            delta = int(self.lonlat_distance((pos[1], pos[0]), (x, y)))
+            if delta <= 50:
+                self.post = '------'
+                self.full_address = \
+                    organization['properties']['CompanyMetaData'][
+                        'name'] + ' ' + \
+                    organization['properties']['CompanyMetaData']['address']
+                r = self.full_address
+                if self.postal_code.isChecked():
+                    r += ' ' + self.post
+                self.adress.setText(r)
+                self.pt = f'{x},{y},comma'
+                self.getImage()
+
+    def get_address(self, address_ll):
+        params = {"apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+                  "geocode": address_ll,
+                  "format": "json"}
+        geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?"
+        response = requests.get(geocoder_request, params=params)
+        adr = ''
+        if response:
+            json_response = response.json()
+            toponym = \
+                json_response["response"]["GeoObjectCollection"][
+                    "featureMember"][
+                    0][
+                    "GeoObject"]
+            try:
+                adr = \
+                    toponym['metaDataProperty']['GeocoderMetaData']['Address'][
+                        'formatted']
+            except Exception:
+                adr = ''
+        return adr
+
     def mousePressEvent(self, event):
         if event.buttons() == Qt.LeftButton:
             x, y = self.to_geo_coords((event.x(), event.y()))
-            self.get_pos(f"{y},{x}")
+            self.pt = self.get_pos(f"{y},{x}", stay=True)
             self.getImage(mouse_req=True)
+        elif event.buttons() == Qt.RightButton:
+            x, y = self.to_geo_coords((event.x(), event.y()))
+            self.find_organiztion((x, y))
+
+    def closeEvent(self, event):
+        if self.widget:
+            self.widget.close()
+
+    def get_help(self):
+        self.widget = QWidget()
+        self.widget.resize(400, 200)
+        self.widget.setWindowTitle('Справка')
+        label = QLabel(self.widget)
+        label.setText(
+            '\n'.join(['Для введения запроса и его отправки, нажимайте Enter',
+                       'Справка по клавишам:',
+                       '* PageUp - приближение',
+                       '* PageDown - отдаление',
+                       '* Стрелочки - перемещение в соответсвующую сторону',
+                       '* 1, 2, 3 - Режимы показа карты',
+                       '* ЛКМ - поиск адреса по нажатию',
+                       '* ПКМ - поиск по организациям']))
+        self.widget.show()
 
 
 def except_hook(cls, exception, traceback):
